cd9d67f00ecb44da0c3830556d918a476090eba5
artjohn1993 <artjohn1993@gmail.com>
2018-02-02 13:16:19 +0800

    comment test 3

--- app/src/main/java/co/blackfintech/instafilter/activities/SplashActivity.kt
+++ app/src/main/java/co/blackfintech/instafilter/activities/SplashActivity.kt
@@ -10,7 +10,7 @@ import kotlinx.android.synthetic.main.activity_splash.*
 import org.jetbrains.anko.intentFor
 
 // Hi, this is a comment.
-//test 2
+//test 3
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 class SplashActivity : AppCompatActivity() {
 
